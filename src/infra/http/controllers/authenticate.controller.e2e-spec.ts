08b0749b6abec870aa0a7b9d344f6fc02003fdc2
killer-cf <costa.kilder@live.com>
2023-09-16 17:13:34 -0300

    refactor e2e tests

--- src/infra/http/controllers/authenticate.controller.e2e-spec.ts
+++ src/infra/http/controllers/authenticate.controller.e2e-spec.ts
@@ -1,36 +1,35 @@
 import { AppModule } from '@/infra/app.module'
 import { BcryptHasher } from '@/infra/cryptography/bcrypt-hasher'
-import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { DatabaseModule } from '@/infra/database/database.module'
 import { INestApplication } from '@nestjs/common'
 import { Test } from '@nestjs/testing'
 import request from 'supertest'
+import { StudentFactory } from 'test/factories/make-student'
 
 describe('Authenticate (e2e)', () => {
   let app: INestApplication
-  let prisma: PrismaService
+  let studentFactory: StudentFactory
   let bcryptHasher: BcryptHasher
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory],
     }).compile()
 
     app = moduleRef.createNestApplication()
 
     bcryptHasher = new BcryptHasher()
 
-    prisma = moduleRef.get(PrismaService)
+    studentFactory = moduleRef.get(StudentFactory)
 
     await app.init()
   })
 
   test('[POST] /sessions', async () => {
-    await prisma.user.create({
-      data: {
-        name: 'jopnh doe',
-        email: 'jonh@doe.com',
-        password: await bcryptHasher.hash('password'),
-      },
+    await studentFactory.makePrismaStudent({
+      email: 'jonh@doe.com',
+      password: await bcryptHasher.hash('password'),
     })
 
     const response = await request(app.getHttpServer()).post('/sessions').send({
