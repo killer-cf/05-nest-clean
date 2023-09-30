3da2cef8fc4c795f241c4fe4d6ba4ba24567a9bb
killer-cf <costa.kilder@live.com>
2023-09-30 17:39:38 -0300

    cache module

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -18,8 +18,10 @@ import { AttachmentsRepository } from '@/domain/forum/aplication/repositories/at
 import { PrismaAttachmentsRepository } from './prisma/repositories/prisma-attachments-repository'
 import { NotificationsRepository } from '@/domain/notification/application/repositories/notifications-repository'
 import { PrismaNotificationRepository } from './prisma/repositories/prisma-notifications-repository'
+import { CacheModule } from '../cache/cache.module'
 
 @Module({
+  imports: [CacheModule],
   providers: [
     PrismaService,
     {
